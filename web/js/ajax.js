963c462d5d712612799819cabcbe5942e4cabb77
linshuna <1487301146@qq.com>
2018-12-07 23:30:48 +0800

    12.07提交web项目

--- web/js/ajax.js
+++ web/js/ajax.js
@@ -2,7 +2,7 @@ $('#send').click(function(){
     $.ajax({
         type: "GET",
         url:" app.json",
-        // data:{"obj":"booksHomeworkList","booksId":"","teacherCode":""},
+        data:{"obj":"booksHomeworkList","booksId":"","teacherCode":""},
         dataType: "json",
         success:function(data){
             console.log(data);
